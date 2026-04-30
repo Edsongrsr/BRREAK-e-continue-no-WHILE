cee58a8873645a2fb4421ceba8e0c96f3ef8e69d
Edsongrsr <al8010774@gmail.com>
2026-04-30 19:19:55 -0300

    Add files via upload

---
+++ index.py
@@ -0,0 +1,91 @@
+# contador = 1
+
+# while contador <= 10 :
+#     print(contador)
+#     if contador == 5:
+#         break
+#     contador +=1
+
+# senha = "" 
+# while true:
+#     senha = input("Digite a senha: ")
+
+#     if senha == "1234":
+#         print("Acesso liberado")
+#         break
+#     print("Senha incorreta")
+
+###Continue
+# numero = 1
+# while numero <= 5:
+#     if numero == 2:
+#         numero +=1
+#         continue
+#     print(numero)
+#     numero +=1
+#Atividade 01
+# numero = 1 
+# while numero <= 10:
+#     print(numero)
+
+#     if numero == 6:
+#         break
+#     numero +=1
+
+#Atividade 02
+# numero = 1
+# while numero <=10:
+#      if numero == 5:
+#           numero += 1
+#           continue
+#      print(numero)
+#      numero+= 1
+
+#Atividade 03
+# numero = 1
+
+# while numero <= 20:
+#     if numero == 17:
+#         break
+#     if numero % 2 == 0:
+#         numero += 1
+#         continue
+#     print (numero)
+#     numero +=1
+
+#Atividade 04
+# while True:
+#     produto = input("Digite o nome do produto:")
+
+#     if produto == "fim":
+#         print("cadastro finalizado")
+#         break
+#     print("Produto Cadastrado", produto)
+
+#Atividade 05
+# soma = 0
+# while True:
+#      numero = int(input("Digite um numero: "))
+#      soma += numero
+
+#      if soma >= 20:
+#           break
+#      print("total", soma)
+
+#Atividade 06
+# soma = 0
+# while True:
+#     numero = int(input("Digite o numero: "))
+#     soma += numero
+
+#     if soma >= 50:
+#         print("limite atingido!")
+#         print("total:", soma)
+#         break
+
+#Atividade 07
+# while True:
+#     senha = input("Digite a senha: ")
+
+#     if senha == "teste":
+#         print("acesso liberado")
